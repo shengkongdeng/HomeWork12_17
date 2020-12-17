5320e4a393aa4456a64c0371328181748bf1303d
Administrator <2291583067@qq.com>
2020-12-17 17:20:43 +0800

    新建了一堆库

---
+++ Mvplibrary/src/main/java/com/example/IBase/IBasePresenter.java
@@ -0,0 +1,10 @@
+package com.example.IBase;
+
+/**
+ * 创建者: 走廊里の声控灯
+ * 项目名: Round_One
+ * 包名: com.example.IBase
+ * 创建时间: 2020/12/17 17:20
+ */
+public interface IBasePresenter {
+}
---
+++ Mvplibrary/src/main/java/com/example/IBase/IBaseView.java
@@ -0,0 +1,10 @@
+package com.example.IBase;
+
+/**
+ * 创建者: 走廊里の声控灯
+ * 项目名: Round_One
+ * 包名: com.example.IBase
+ * 创建时间: 2020/12/17 17:19
+ */
+public interface IBaseView {
+}
